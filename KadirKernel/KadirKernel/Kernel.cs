b11ff8ad5d3fa311df977f7a9fe91b7177217eb3
Hubertus Sack <hubertussack@outlook.com>
2015-02-15 02:36:00 +0100

    Create a small running example

---
+++ KadirKernel/KadirKernel/Kernel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+
+namespace KadirKernel
+{
+    public class Kernel : Sys.Kernel
+    {
+        private string path;
+
+        protected override void BeforeRun()
+        {
+            Console.WriteLine("Starting Kadir...");
+            Console.WriteLine();
+            Console.WriteLine();
+            path = "C:\\";
+        }
+
+        protected override void Run()
+        {
+            Console.Write(path + ">");
+            var input = Console.ReadLine();
+            var line = input.Trim();
+            switch (line)
+            {
+                case "ver":
+                    Console.WriteLine();
+                    Console.WriteLine("Kadir Version 0.1");
+                    Console.WriteLine();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
